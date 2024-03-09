48aae4706492877f6cc3c29c044d17a6de1dc3a3
Ben Gumaan <bot.s8x@gmail.com>
2024-03-09 12:08:25 +0100

    Final website

--- src/data.js
+++ src/data.js
@@ -10,7 +10,10 @@
 */
 
 export const quizData = {
+  totalPoints: 15,
   currentQuestionIndex: 0,
+  score: 0,
+  questionsTracker: [],
   // the questions in the quiz
   questions: [
     {
@@ -19,6 +22,7 @@ export const quizData = {
         a: 'constant, let, variable',
         b: 'var, const, let, function',
         c: 'var, let, const',
+        d: 'xyz, xyz, xyz',
       },
       correct: 'c',
       selected: null,
@@ -50,11 +54,283 @@ export const quizData = {
         },
         {
           text: 'MDN',
-          href:
-            'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof',
+          href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof',
+        },
+      ],
+    },
+    {
+      text: 'rrrrrrrrrrrrrrrrrrrrrrrrrrr',
+      answers: {
+        a: 'changes the type of a primitive value',
+        b: 'returns a string describing the type of a value',
+        c: 'determines if a value is primitive',
+        d: 'can tell the difference between arrays and objects',
+      },
+      correct: 'b',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/types#type-typeof',
+        },
+        {
+          text: 'MDN',
+          href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof',
+        },
+      ],
+    },
+    {
+      text: 'Wwwwwwwwwwwwwwwwwwwwwwwwwww',
+      answers: {
+        a: 'changes the type of a primitive value',
+        b: 'returns a string describing the type of a value',
+        c: 'determines if a value is primitive',
+        d: 'can tell the difference between arrays and objects',
+      },
+      correct: 'b',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/types#type-typeof',
+        },
+        {
+          text: 'MDN',
+          href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof',
+        },
+      ],
+    },
+    {
+      text: 'Wccccccccccccccccccccccccccccccccc',
+      answers: {
+        a: 'changes the type of a primitive value',
+        b: 'returns a string describing the type of a value',
+        c: 'determines if a value is primitive',
+        d: 'can tell the difference between arrays and objects',
+      },
+      correct: 'b',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/types#type-typeof',
+        },
+        {
+          text: 'MDN',
+          href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof',
+        },
+      ],
+    },
+    {
+      text: 'xxxxxxxxxxxxxxxxxxxxxxx',
+      answers: {
+        a: 'changes the type of a primitive value',
+        b: 'returns a string describing the type of a value',
+        c: 'determines if a value is primitive',
+        d: 'can tell the difference between arrays and objects',
+      },
+      correct: 'b',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/types#type-typeof',
+        },
+        {
+          text: 'MDN',
+          href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof',
+        },
+      ],
+    },
+    {
+      text: 'zzzzzzzzzzzzzzzzzzzzzzzzzz',
+      answers: {
+        a: 'changes the type of a primitive value',
+        b: 'returns a string describing the type of a value',
+        c: 'determines if a value is primitive',
+        d: 'can tell the difference between arrays and objects',
+      },
+      correct: 'b',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/types#type-typeof',
+        },
+        {
+          text: 'MDN',
+          href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof',
         },
       ],
     },
     // Add more questions here
+    {
+      text: '	Which of the following laws is associated with the heating effect of electric current ?',
+      answers: {
+        a: "Joule's law",
+        b: "Ohm's law ",
+        c: "Faraday's law ",
+        d: "Newton's law ",
+      },
+      correct: 'd',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: 'Who demonstrated that objects of different masses would reach the ground together when dropped from the same height?',
+      answers: {
+        a: 'Isaac Newton',
+        b: 'Archimedes ',
+        c: 'Robert Boyle ',
+        d: 'Galileo Galilei ',
+      },
+      correct: 'a',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: '	A body at rest can have ?',
+      answers: {
+        a: 'speed',
+        b: 'velocity',
+        c: 'momentum',
+        d: 'Energy',
+      },
+      correct: 'a',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: '	The pH of pure water is ?',
+      answers: {
+        a: '2',
+        b: '6',
+        c: '7',
+        d: '8',
+      },
+      correct: 'd',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: 'What is the ability of a metal to be drawn into thin wires called ?',
+      answers: {
+        a: 'Ductility',
+        b: 'Sonorous ',
+        c: 'Malleability',
+        d: 'Intractability',
+      },
+      correct: 'd',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: 'The sex of the children will be determined by what they inherit from the ?',
+      answers: {
+        a: 'nature',
+        b: 'father',
+        c: 'mother',
+        d: 'mother and father',
+      },
+      correct: 'a',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: 'If the mass of a solid is doubled, its density ?',
+      answers: {
+        a: 'becomes two times',
+        b: 'becomes half ',
+        c: 'remains unchanged',
+        d: 'becomes four times',
+      },
+      correct: 'b',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: 'Fats and oils become rancid because of ?',
+      answers: {
+        a: 'transpiration ',
+        b: 'reduction ',
+        c: 'oxidation',
+        d: 'corrosion ',
+      },
+      correct: 'a',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
   ],
 };
--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -1,11 +1,15 @@
 import {
   ANSWERS_LIST_ID,
   NEXT_QUESTION_BUTTON_ID,
+  RESTART_QUIZ_BUTTON_ID,
   USER_INTERFACE_ID,
 } from '../constants.js';
 import { createQuestionElement } from '../views/questionView.js';
 import { createAnswerElement } from '../views/answerView.js';
 import { quizData } from '../data.js';
+import { showResultPage } from '../pages/resultPage.js';
+import { LOCAL_STORAGE_NAME } from '../constants.js';
+import { restartQuiz } from '../pages/resultPage.js';
 
 export const initQuestionPage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
@@ -13,24 +17,102 @@ export const initQuestionPage = () => {
 
   const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
 
-  const questionElement = createQuestionElement(currentQuestion.text);
+  const questionElement = createQuestionElement(
+    currentQuestion.text,
+    quizData.currentQuestionIndex,
+    quizData.score
+  );
 
   userInterface.appendChild(questionElement);
 
   const answersListElement = document.getElementById(ANSWERS_LIST_ID);
+  answersListElement.classList.add('answers-list');
 
   for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
     const answerElement = createAnswerElement(key, answerText);
+    // Answers options look little bit better now
+    answerElement.classList.add('answer-element');
+    answerElement.setAttribute('data-key', key);
     answersListElement.appendChild(answerElement);
+
+    answerElement.addEventListener('click', function clickHandler() {
+      // When you click the answer, previous selection loses selection color
+      const answerElements = document.querySelectorAll('.answer-element');
+      answerElements.forEach((answer) => {
+        answer.classList.remove(
+          'answer-selected',
+          'answer-correct',
+          'answer-incorrect'
+        );
+      });
+
+      // push the questions to localStorage to maintain the state even after refreshing (reloading) the page.
+      pushToLocalStorage(currentQuestion, key);
+
+      // After this answers become inactive
+      const answerInactive = function () {
+        answerElements.forEach((answer) => {
+          if (answer.dataset.key === currentQuestion.correct) {
+            answer.classList.add('answer-correct-value');
+            answer.firstElementChild.classList.add('answer-correct-key');
+          }
+          answer.classList.add('nohover');
+          answer.style.pointerEvents = 'none';
+        });
+      };
+
+      const answerCorrect = function () {
+        quizData.score++;
+        answerInactive();
+      };
+
+      const answerIncorrect = function () {
+        answerElement.classList.add('answer-incorrect-value');
+        answerElement.firstElementChild.classList.add('answer-incorrect-key');
+        answerInactive();
+      };
+
+      if (key === currentQuestion.correct) {
+        answerCorrect();
+      } else {
+        answerIncorrect();
+      }
+    });
   }
 
   document
     .getElementById(NEXT_QUESTION_BUTTON_ID)
     .addEventListener('click', nextQuestion);
+
+  document
+    .getElementById(RESTART_QUIZ_BUTTON_ID)
+    .addEventListener('click', restartQuiz);
+};
+
+const pushToLocalStorage = (question = null, answer = null) => {
+  quizData.questionsTracker.push({
+    question: question.text,
+    answer: answer,
+    isCorrect: answer === question.correct ? true : false,
+  });
+
+  localStorage.setItem(
+    LOCAL_STORAGE_NAME,
+    JSON.stringify(quizData.questionsTracker)
+  );
 };
 
 const nextQuestion = () => {
   quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
-
-  initQuestionPage();
+  if (quizData.currentQuestionIndex !== quizData.questionsTracker.length) {
+    pushToLocalStorage(
+      quizData.questions[quizData.currentQuestionIndex - 1],
+      null
+    );
+  }
+  if (quizData.currentQuestionIndex === quizData.questions.length) {
+    showResultPage();
+  } else {
+    initQuestionPage();
+  }
 };
